e57661d409a7b052041c9319ba88e5ad4d1f00c3
arijkhan94 <56958669+arijkhan94@users.noreply.github.com>
2023-09-18 17:30:18 +0000

    datatypes

---
+++ 01/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code on new version
+
+let name="Arij" //string
+
+let age=12  //integer
+
+let isLoggedIn=False  //Boolean type
+
+// bigint
+// string ==>""
+//number ==> 2
+//boolean == true/false
+//null Standalone value
+//undefined
+//symbol uniquness k liya
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
