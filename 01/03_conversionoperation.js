e57d75b58c4c545a79d457aa73a2318280ecd42d
arijkhan94 <56958669+arijkhan94@users.noreply.github.com>
2023-09-19 07:22:24 +0000

    conversion of datatype

---
+++ 01/03_conversionoperation.js
@@ -0,0 +1,15 @@
+let score="33"
+//console.log(typeof score)
+
+let numberIn=Number(score)
+
+//console.log(typeof numberIn)
+
+//33==>33
+//33abc==>NaN==type number hi hy
+//true ==1; falso=0
+
+let isloggedin=1;
+let booleanloggedin=Boolean(isloggedin)
+
+console.log(booleanloggedin)
