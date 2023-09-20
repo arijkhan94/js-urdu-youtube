0df0e67726d72385ce0256162ae556e93cd65a57
arijkhan94 <56958669+arijkhan94@users.noreply.github.com>
2023-09-20 07:49:30 +0000

    datatypesummary

--- 01/03_conversionoperation.js
+++ 01/03_conversionoperation.js
@@ -9,7 +9,30 @@ let numberIn=Number(score)
 //33abc==>NaN==type number hi hy
 //true ==1; falso=0
 
-let isloggedin=1;
-let booleanloggedin=Boolean(isloggedin)
+// let isloggedin=1;
+// let booleanloggedin=Boolean(isloggedin)
 
-console.log(booleanloggedin)
+// console.log(booleanloggedin)
+
+//***************Operations******************
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+let str1="Hello"
+let str2=" Aarij"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+console.log(undefined>=0)
+console.log(undefined>=0)
+console.log(undefined>=0)
---
+++ 01/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive datatypes
+
+// 7 types String Number Boolean Null undefined Symbol BigInt
+
+//Reference (Non Prmitive)
+
+// const score=100;
+// const scorevalue=100.3;
+// const IsLoggedIn=True;
+// const outSideTemp=null;
+// let userEmail;
+
+const bigNumber=12121321321321n;
+
+const id=Symbol('123');
+const anotherid=Symbol('123')
+console.log(id==anotherid);
+
+
+//Array Objects Functions
+
+const heros=["Quaid","Allamaiqbal"];
+let myObj= {
+    name:"aarij",
+    age:"29",
+
+}
+
+const myFunction=function(){
+    console.log("Hello World");
+
+}
+
+//null ka typeof sa object ata hy
+// https://262.ecma-international.org/5.1/#sec-11.4.3
